85de87269bb10ae0fd171fdfa1da0522ce037f64
profecertus <84884709+profecertus@users.noreply.github.com>
2026-08-05 20:13:23 -0500

    Sesion14

--- tareas/src/shared/reloj/reloj.ts
+++ tareas/src/shared/reloj/reloj.ts
@@ -1,17 +1,30 @@
+import { DatePipe } from '@angular/common';
 import { Component, OnDestroy, OnInit, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatTimepickerModule } from '@angular/material/timepicker';
 
 @Component({
   selector: 'app-reloj',
-  imports: [],
+  imports: [
+    DatePipe,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatTimepickerModule,
+  ],
+  providers: [provideNativeDateAdapter()], //Importante para que funcione el MatTimepickerModule
   templateUrl: './reloj.html',
   styleUrl: './reloj.css',
 })
 export class Reloj implements OnInit, OnDestroy {
   private timer: any;
-  hora = signal('');
+  hora = signal<Date>(new Date());
 
   ngOnInit(): void {
-    this.timer = setInterval(() => this.hora.set(new Date().toLocaleTimeString()), 1000);
+    this.timer = setInterval(() => this.hora.set(new Date()), 1000);
   }
 
   ngOnDestroy(): void {    
