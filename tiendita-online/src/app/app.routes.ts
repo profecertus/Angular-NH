6399c8f1bf230443cb490225c773847f4c71b555
profecertus <84884709+profecertus@users.noreply.github.com>
2026-08-14 19:12:54 -0500

    fix: corregir config del router, rutas faltantes y template 404

--- tiendita-online/src/app/app.routes.ts
+++ tiendita-online/src/app/app.routes.ts
@@ -4,16 +4,19 @@ import { Catalogo } from './features/catalogo/catalogo';
 import { DetalleProducto } from './features/catalogo/detalle-producto';
 import { Carrito } from './features/carrito/carrito';
 import { Login } from './features/login/login';
+import { NoEncontrado } from './features/no-encontrado/no-encontrado';
 
 export const routes: Routes = [
     {path:'', component:Inicio, title: 'Tiendita-Inicio', pathMatch:'full'},
     {path:'catalogo', component:Catalogo, title:'Tiendita-Catalogo'},
     {path:'catalogo/:id', component:DetalleProducto, title:'Tiendita-Detalle Producto'},
     {path:'carrito', component:Carrito, title:'Tiendita-Carrito'},
+    {path:'checkout', loadComponent: () => import('./features/checkout/checkout').then(m => m.Checkout), title:'Tiendita-Checkout'},
     {path:'login', component:Login, title:'Tiendita-Login'},
     {
         path:'admin',
         loadChildren: () =>
             import('./features/admin/admin.routes').then(m => m.ADMIN_ROUTES),
-    }
+    },
+    {path:'**', component:NoEncontrado, title:'Tiendita-404'}
 ];
